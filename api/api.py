7034fdd0d42d05b08cacfb5d0dfad0b84079f49c
Lauren Maghran <maghranlauren@gmail.com>
2020-04-10 16:02:29 -0700

    working to display one book

--- api/api.py
+++ api/api.py
@@ -1,8 +1,42 @@
 import time
-from flask import Flask
+from flask import Flask, render_template, json
+import requests
+from bs4 import BeautifulSoup as bs
 
 app = Flask(__name__)
 
+# @app.route('/time')
+# def get_current_time():
+#     return {'time': time.time()}
+
 @app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+def booklist_api():
+    """API for Booklist"""
+
+    text = requests.get("https://www.gutenberg.org/browse/scores/top#books-last7")
+    soup= bs(text.content, 'html.parser')
+    last_30 = soup.find(id = "books-last30")
+    siblings = list(last_30.findNextSiblings())
+    book_list= []
+    book_array=[]
+    for ul in siblings:
+        for li in ul.findAll('li'):
+            book_list.append(li)
+    # removing the ul element from the list
+    book_list.pop(0)
+
+    for li in book_list[:1]:
+        book_dict={}
+        book_title_auth= li.text.split("by", 1)
+        book_title= book_title_auth[0]
+        if len(book_title_auth)>1:
+            book_auth= book_title_auth[1][:-7]
+        else:
+            book_auth= "No author"
+        book_url=li.findAll('a')[0]['href']
+        book_dict = {"title": book_title, "url":book_url, "author":book_auth}
+    
+        book_array.append(book_dict)
+    book_array= json.dumps(book_dict)
+
+    return book_array
--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentBook, setCurrentBook] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentBook(data.title);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>The first book is {currentBook}.</p>
       </header>
     </div>
   );
